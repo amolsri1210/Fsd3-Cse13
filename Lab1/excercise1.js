3574f958b4ee1d0feb08323d2617673ff48cb8db
amolsri1210 <amol.srivastava.1205@gmail.com>
2026-07-22 11:12:41 +0530

    Add hello.js with greeting and sum functions

---
+++ Lab1/excercise1.js
@@ -0,0 +1,7 @@
+//wap to create function  number between 0 to 9 and return in words
+const words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+const numberToWord = (num) => {
+    return words[num];
+};
+console.log(numberToWord(8)); 
